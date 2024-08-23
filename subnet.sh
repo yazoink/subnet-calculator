fcc0f1ac5a4b19bab6757e195210aee9b18e5278
yazoink <yazoink@firemail.cc>
2024-08-23 20:13:32 +1000

    loop until user exits

--- subnet.sh
+++ subnet.sh
@@ -10,45 +10,55 @@ main() {
     fi
 
     while [[ 1 ]]; do
-        read -p "Enter number of subnets ($MIN_SUBNET-$MAX_SUBNET): " subnetNum
-        if isNum "$subnetNum"; then
-            if (($subnetNum >= $MIN_SUBNET && $subnetNum <= $MAX_SUBNET)); then
-                break
+        while [[ 1 ]]; do
+            read -p "Enter number of subnets ($MIN_SUBNET-$MAX_SUBNET): " subnetNum
+            if isNum "$subnetNum"; then
+                if (($subnetNum >= $MIN_SUBNET && $subnetNum <= $MAX_SUBNET)); then
+                    break
+                fi
             fi
+        done
+        
+        borrowedBits=0
+        result=$((2 ** borrowedBits))
+        while [[ $result -lt $subnetNum ]]; do
+            borrowedBits=$((borrowedBits + 1))
+            result=$((2 ** borrowedBits))
+        done
+
+        binary=(0 0 0 0 0 0 0 0)
+        for ((i=0; i<$borrowedBits; i++)); do
+            binary[i]=1
+        done
+        
+        hostBits=$((8 - borrowedBits))
+        addressesPerSubnet=$((2 ** hostBits))
+        hostsPerSubnet=$((addressesPerSubnet - 2))
+        subnetMask=$((255 - addressesPerSubnet + 1))
+        cidr=$((24 + borrowedBits))
+
+        printf "Bits Borrowed: %d\n" $borrowedBits
+        
+        printf "Binary last Byte in SNM: "
+        for elem in ${binary[@]}; do
+            printf "%d" $elem
+        done
+        printf "\n"
+
+        printf "Subnets: %d\n" $subnetNum
+        printf "Addresses Per Subnet: %d\n" $addressesPerSubnet
+        printf "Hosts Per Subnet: %d\n" $hostsPerSubnet
+        printf "Subnet Mask: .%d\n" $subnetMask
+        printf "CIDR: /%d\n" $cidr
+
+        yesOrNo='a'
+        while [[ "${yesOrNo^^}" != "YES" ]] && [[ "${yesOrNo^^}" != "Y" ]] && [[ "${yesOrNo^^}" != "NO" ]] && [[ "${yesOrNo^^}" != "N" ]] && [[ "${yesOrNo^^}" != "" ]]; do
+            read -p "Continue? (Y/n): " yesOrNo
+        done
+        if [[ "${yesOrNo^^}" = "NO" ]] || [[ "${yesOrNo^^}" = "N" ]]; then
+            exit 0
         fi
     done
-    
-    borrowedBits=0
-    result=$((2 ** borrowedBits))
-    while [[ $result -lt $subnetNum ]]; do
-        borrowedBits=$((borrowedBits + 1))
-        result=$((2 ** borrowedBits))
-    done
-
-    binary=(0 0 0 0 0 0 0 0)
-    for ((i=0; i<$borrowedBits; i++)); do
-        binary[i]=1
-    done
-    
-    hostBits=$((8 - borrowedBits))
-    addressesPerSubnet=$((2 ** hostBits))
-    hostsPerSubnet=$((addressesPerSubnet - 2))
-    subnetMask=$((255 - addressesPerSubnet + 1))
-    cidr=$((24 + borrowedBits))
-
-    printf "Bits Borrowed: %d\n" $borrowedBits
-    
-    printf "Binary last Byte in SNM: "
-    for elem in ${binary[@]}; do
-        printf "%d" $elem
-    done
-    printf "\n"
-
-    printf "Subnets: %d\n" $subnetNum
-    printf "Addresses Per Subnet: %d\n" $addressesPerSubnet
-    printf "Hosts Per Subnet: %d\n" $hostsPerSubnet
-    printf "Subnet Mask: .%d\n" $subnetMask
-    printf "CIDR: /%d\n" $cidr
 }
 
 help() {
